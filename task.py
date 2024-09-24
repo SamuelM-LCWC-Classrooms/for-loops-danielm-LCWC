1471b83b0922bd82986f1007c879eddce5a39fce
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-09-24 13:06:23 +0000

    I think I did everything right. I did this faster than I expected

--- task.py
+++ task.py
@@ -5,7 +5,8 @@ def task_1(): # Lottery ticket generator
     ticket = []
 
     # Code here
-    
+    for i in range(6):
+        ticket.append(randrange(1, 50))
     return ticket
 
 def task_2(): # Countdown
@@ -13,7 +14,12 @@ def task_2(): # Countdown
     output = []
 
     # Code here
-
+    number = int(input("Enter a number lower than 100: "))
+    countdown = 100
+    for i in range(countdown - number + 1):
+        output.append(countdown)
+        print(countdown)
+        countdown -= 1
     return output
 
 def task_3():
@@ -27,7 +33,12 @@ def task_3():
     }
 
     car_make_lengths = ()
-
     # Code here
-
-    return # Code the sentence here
+    car_make_lengths2 = []
+    i = 0
+    for car in people_cars.values():
+        x = len(car)
+        if not x in car_make_lengths2:
+            car_make_lengths2.append(x)
+    car_make_lengths = car_make_lengths2
+    return car_make_lengths
